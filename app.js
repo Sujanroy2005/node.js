7c8bf3cb07e1ff5861c08a667a96c7351691ca70
Sujanroy2005 <2022sujanroy@gmail.com>
2026-09-01 17:59:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const http = require('http);
-const server = http.createserver((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain'});
-  res.end('Hello from Node.js on Google Cloud!\n);
-          });
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Node.js on Google Cloud!\n');
+});
+
 const PORT = process.env.PORT || 8080;
+
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
